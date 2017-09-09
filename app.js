3dd55cdef767230488a8f7921c5a457bc4f0533e
Kris <kris.weber.1994@gmail.com>
2017-09-09 13:53:59 -0700

    install nodemon, add initial index route

--- app.js
+++ app.js
@@ -22,9 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
-
+app.get('/', (req, res) => {
+  res.send('This is the index page');
+})
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
