e908d18e63bae96d86557af0f2b683647a8165a9
Kris <kris.weber.1994@gmail.com>
2017-09-09 14:03:09 -0700

    Add folder for tests and controllers

--- app.js
+++ app.js
@@ -23,17 +23,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.send('This is the index page');
-})
+  res.send({
+    name: 'Kris',
+    runner: true
+  });
+});
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
