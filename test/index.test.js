b7a58405f2d2cf47e51ef8e8f3ea9b2a84dfc317
Kris <kris.weber.1994@gmail.com>
2017-09-09 18:43:42 -0700

    Install expect and supertest; write first test for index route, checking the status code

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,10 @@
+const expect = require('expect');
+const request = require('supertest');
 const app = require('../app');
 
-it('should render the index page', () => {
-  
+it('should render the index page', (done) => {
+  request(app)
+    .get('/')
+    .expect(200)
+    .end(done);
 })
