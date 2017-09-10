7d35ccd7b89607f405dc9ce7dc2e975cd09c3097
Kris <kris.weber.1994@gmail.com>
2017-09-10 13:48:11 -0700

    Extract routes from main app file

--- app.js
+++ app.js
@@ -21,13 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
-
-app.get('/about', (req, res) => {
-  res.render('about.ejs');
-});
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   var err = new Error('Not Found');
---
+++ routes/about.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/about', (req, res) => {
+  res.render('about.ejs');
+});
