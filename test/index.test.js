a154d23f57949788557658993251c52acc39967c
Kris <kris.weber.1994@gmail.com>
2017-09-18 12:15:05 -0700

    Add first few tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,11 +2,56 @@ const expect = require('expect');
 const request = require('supertest');
 const app = require('../app');
 
-it('should render the index page', (done) => {
+it('should return a status code of 200', (done) => {
   request(app)
     .get('/')
     .expect((res) => {
       expect(res.status).toEqual(200)
+    })
+    .end(done);
+})
+
+it('should render the index page', (done) => {
+  request(app)
+    .get('/')
+    .expect((res) => {
+      // test goes here
+    })
+    .end(done);
+})
+
+it('should ', (done) => {
+  request(app)
+    .get('/')
+    .expect((res) => {
+      // test goes here
+    })
+    .end(done);
+})
+
+it('should ', (done) => {
+  request(app)
+    .get('/')
+    .expect((res) => {
+      // test goes here
+    })
+    .end(done);
+})
+
+it('should ', (done) => {
+  request(app)
+    .get('/')
+    .expect((res) => {
+      // test goes here
+    })
+    .end(done);
+})
+
+it('should ', (done) => {
+  request(app)
+    .get('/')
+    .expect((res) => {
+      // test goes here
     })
     .end(done);
 })
