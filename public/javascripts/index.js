e8cb54605b9436a97be8f889ebbfa5b6bd4d0316
Kris <kris.weber.1994@gmail.com>
2017-09-19 16:50:59 -0700

    Add comments

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,11 @@
+// No longer needed, as the Weather Underground API provides weather information for the location of the user's IP address
+
 $(document).ready(function() {
   var app = '../../app';
   var lat;
   var lon;
+
+  // Getting the coordinates of the user's current location
   $.getJSON('https://ipinfo.io/geo', function(response) {
     var loc = response.loc.split(',');
     var coords = {
